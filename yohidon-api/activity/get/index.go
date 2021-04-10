32485b157393d6f8e05d75ac8fa1baaa6edf751c
Kento Okamura <kentoooooo.1230@gmail.com>
2021-04-11 01:40:57 +0900

    rename study-log to activity

--- yohidon-api/activity/get/index.go
+++ yohidon-api/activity/get/index.go
@@ -18,11 +18,11 @@ var (
 	PASSWORD = os.Getenv("YOHIDON_DB_PASSWORD")
 )
 
-type StudyLogs struct {
-	List []studyLog `json:"studyLogs"`
+type Activities struct {
+	List []activity `json:"activities"`
 }
 
-type studyLog struct {
+type activity struct {
 	UserId       string    `json:"userId"`
 	UserName     string    `json:"userName"`
 	CategoryId   string    `json:"categoryId"`
@@ -37,18 +37,18 @@ func Handler(w http.ResponseWriter, req *http.Request) {
 	sqlStatement := "select u.user_id, u.user_name, c.category_id, s.time, s.created, c.category_name from users u left join study_log s on u.user_id = s.user_id left join category c on c.category_id = s.category_id order by s.created desc limit 20;"
 	rows, _ := db.Query(sqlStatement)
 
-	var studyLogs []studyLog
+	var activities []activity
 
 	for rows.Next() {
-		studyLog := &studyLog{}
-		if err := rows.Scan(&studyLog.UserId, &studyLog.UserName, &studyLog.CategoryId, &studyLog.StudyTime, &studyLog.Created, &studyLog.CategoryName); err != nil {
+		activity := &activity{}
+		if err := rows.Scan(&activity.UserId, &activity.UserName, &activity.CategoryId, &activity.StudyTime, &activity.Created, &activity.CategoryName); err != nil {
 			log.Println(err)
 			break
 		}
-		studyLogs = append(studyLogs, *studyLog)
+		activities = append(activities, *activity)
 	}
 
 	w.Header().Set("Content-Type", "application/json")
-	bytes, _ := json.Marshal(StudyLogs{studyLogs})
+	bytes, _ := json.Marshal(Activities{activities})
 	fmt.Fprint(w, string(bytes))
 }
