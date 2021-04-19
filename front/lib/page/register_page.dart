d9551e6a16d0cc9f010c11795dff5a5957987df1
Kento Okamura <kentoooooo.1230@gmail.com>
2021-04-19 13:00:12 +0900

    add dialog

--- front/lib/page/register_page.dart
+++ front/lib/page/register_page.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:yohidon/domain/category.dart';
@@ -20,81 +21,130 @@ class RegisterPage extends StatelessWidget {
       padding: EdgeInsets.all(24),
       child: Center(
         child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              TextField(
-                decoration: InputDecoration(
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.all(Radius.circular(12))
-                    ),
-                    labelText: 'カテゴリー名',
-                ),
-                onChanged: (input) {
-                  getIt<CategoryRegistrationUsecase>().inputCategoryName(CategoryName(input));
-                  },
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            TextField(
+              decoration: InputDecoration(
+                border: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(12))),
+                labelText: 'カテゴリー名',
               ),
-              ElevatedButton(
-                child: Text('カテゴリー登録'),
-                style: ElevatedButton.styleFrom(primary: Colors.orange, onPrimary: Colors.white),
-                onPressed: () {
-                  getIt<CategoryRegistrationUsecase>().execute(CategoryName(state.categoryName));
-                },
+              onChanged: (input) {
+                getIt<CategoryRegistrationUsecase>()
+                    .inputCategoryName(CategoryName(input));
+              },
+            ),
+            ElevatedButton(
+              child: Text('カテゴリー登録'),
+              style: ElevatedButton.styleFrom(
+                primary: Colors.orange,
+                onPrimary: Colors.white,
+                minimumSize: Size(150, 50),
               ),
-              Divider(
-                thickness: 2.0,
-              ),
-              Container(
-                child: DropdownButton(
-                  value: state.selectedItem,
-                  items: state.selectItems?.values
-                      .map((e) => DropdownMenuItem(value: e, child: Text(e.name)))
-                      .toList(),
-                  onChanged: (SelectItem? item) => {
-                    getIt<SelectCategoryUsecase>().execute(item!.toCategory())
-                  },
-                ),
-              ),
-              TextField(
-                textInputAction: TextInputAction.newline,
-                maxLines: 3,
-                decoration: InputDecoration(
-                  hintText: 'メモ',
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.all(Radius.circular(12))
-                  ),
-                ),
-                onChanged: (input) {
-                  getIt<ChangeMemoUsecase>().execute(Memo(input));
-                },
-              ),
-              Slider(
-                value: state.rating,
-                onChanged: (newRating) {
-                  getIt<ChangeSliderUsecase>().execute(StudyTime(newRating));
-                },
-                divisions: 24,
-                min: 0.0,
-                max: 12.0,
-                label: "${state.rating}",
-              ),
-              ElevatedButton(
-                  child: Text('記録追加'),
-                  style: ElevatedButton.styleFrom(
-                      primary: Colors.orange, onPrimary: Colors.white),
-                  onPressed: () {
-                    getIt<StudyRegisterUsecase>().execute(
-                      StudyLog(
-                        Category(
-                          CategoryId(state.selectedItem!.id),
-                          CategoryName(state.selectedItem!.name),
-                        ),
-                        Memo(state.memo),
-                        StudyTime(state.rating),
+              onPressed: () {
+                showCupertinoDialog(
+                  context: context,
+                  builder: (a) => CupertinoAlertDialog(
+                    content: Text("カテゴリー「${state.categoryName}」を追加します"),
+                    actions: [
+                      CupertinoDialogAction(
+                        child: Text("No"),
+                        onPressed: () {
+                          Navigator.of(context).pop();
+                        },
                       ),
-                    );
-                  })
-            ]),
+                      CupertinoDialogAction(
+                        child: Text("Yes"),
+                        onPressed: () {
+                          getIt<CategoryRegistrationUsecase>()
+                              .execute(CategoryName(state.categoryName));
+                          Navigator.of(context).pop();
+                        },
+                      ),
+                    ],
+                  ),
+                );
+              },
+            ),
+            Divider(
+              thickness: 2.0,
+            ),
+            Container(
+              child: DropdownButton(
+                value: state.selectedItem,
+                items: state.selectItems?.values
+                    .map((e) => DropdownMenuItem(value: e, child: Text(e.name)))
+                    .toList(),
+                onChanged: (SelectItem? item) => {
+                  getIt<SelectCategoryUsecase>().execute(item!.toCategory())
+                },
+              ),
+            ),
+            TextField(
+              textInputAction: TextInputAction.done,
+              maxLines: 3,
+              decoration: InputDecoration(
+                hintText: 'メモ',
+                border: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(12))),
+              ),
+              onChanged: (input) {
+                getIt<ChangeMemoUsecase>().execute(Memo(input));
+              },
+            ),
+            Slider(
+              value: state.rating,
+              onChanged: (newRating) {
+                getIt<ChangeSliderUsecase>().execute(StudyTime(newRating));
+              },
+              divisions: 24,
+              min: 0.0,
+              max: 12.0,
+              label: "${state.rating}",
+            ),
+            ElevatedButton(
+              child: Text('記録追加'),
+              style: ElevatedButton.styleFrom(
+                  primary: Colors.orange,
+                  onPrimary: Colors.white,
+                  minimumSize: Size(150, 50)),
+              onPressed: () {
+                showCupertinoDialog(
+                  context: context,
+                  builder: (_) => CupertinoAlertDialog(
+                    content: Text(
+                        "${state.selectedItem!.name} ${state.rating}時間で登録します"),
+                    actions: [
+                      CupertinoDialogAction(
+                        child: Text("No"),
+                        onPressed: () {
+                          Navigator.of(context).pop();
+                        },
+                      ),
+                      CupertinoDialogAction(
+                        child: Text("Yes"),
+                        onPressed: () {
+                          getIt<StudyRegisterUsecase>().execute(
+                            StudyLog(
+                              Category(
+                                CategoryId(state.selectedItem!.id),
+                                CategoryName(state.selectedItem!.name),
+                              ),
+                              Memo(state.memo),
+                              StudyTime(state.rating),
+                            ),
+                          );
+                          Navigator.of(context).pop();
+                        },
+                      ),
+                    ],
+                  ),
+                );
+              },
+            ),
+          ],
+        ),
       ),
     );
   }
